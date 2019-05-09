34c02bb680a5252770bb1e170d6a42bd0b647c41
Andereoo <50446686+Andereoo@users.noreply.github.com>
2019-05-09 13:14:13 -0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'Small cross-platform music note player for beginners to learn how to manipulate system uses',
   author = 'Andrew',
   url = 'https://github.com/Andereoo/PythonMusic',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Andereoo/PythonMusic/archive/v_0.1.tar.gz',    # I explain this later on
   keywords = ['Python', 'Music'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',
